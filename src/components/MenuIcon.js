b4e8550d793d8c4ba9a370621367b8f3da314e46
berhangorkemcoskun <coskunberhangorkem@gmail.com>
2024-05-08 12:34:20 +0300

    TopBar fixed

---
+++ src/components/MenuIcon.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { Icon } from "react-native-elements";
+
+const MenuIcon = () => {
+  return (
+    <TouchableOpacity>
+        <Icon name="menu" color="black"/>
+    </TouchableOpacity>
+  );
+};
+
+
+export default MenuIcon;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,25 +1,34 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { Text } from "react-native-elements";
+import { Header, Icon } from "react-native-elements";
+import MenuIcon from "./MenuIcon"
 
-const TopBar = ({text}) => {
+const TopBar = ({ text }) => {
   return (
-    <View style={styles.backgroundStyle}>
-      <Text h5>{text}</Text>
-    </View>
+    <Header
+      containerStyle={{
+        backgroundColor: "#D9D9D9",
+        justifyContent: "space-around",
+        marginTop: 30,
+      }}
+      leftComponent={
+        <Icon name="leaf" type="material-community" color="black" />
+      }
+      centerComponent={{ text: text, style: { color: "black" } }}
+      rightComponent={<MenuIcon/>}
+    />
   );
 };
 
 const styles = StyleSheet.create({
   backgroundStyle: {
-    justifyContent:"center",
-    alignItems:"center",
+    justifyContent: "center",
+    alignItems: "center",
     height: 40,
     marginTop: 40,
-    marginBottom:10,
-    borderBottomWidth:1
+    marginBottom: 10,
+    borderBottomWidth: 1,
   },
-
 });
 
 export default TopBar;
